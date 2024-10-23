e42cc23e931b95ab28a3256ac72f65347e29fbf8
Yassine Khindouf <ykhindou@c3r9p9.1337.ma>
2024-10-23 08:27:36 +0100

    third day of libft just started

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ykhindou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:18:45 by ykhindou          #+#    #+#             */
-/*   Updated: 2024/10/22 12:21:42 by ykhindou         ###   ########.fr       */
+/*   Updated: 2024/10/22 19:48:44 by ykhindou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 #define LIBFT_H
 
 size_t	ft_strlen(const char *s);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif
